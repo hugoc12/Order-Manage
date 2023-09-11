f92f081c018e689abbf8291c2e2325264f2ae18f
hugoc12 <hugo_c12@outlook.com>
2023-09-11 14:27:36 -0300

    bootstrap install!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import {Button} from 'react-bootstrap'
+
 export default function App(){
   return(
     <div>
       <h1>Olá Vite</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   )
 }
