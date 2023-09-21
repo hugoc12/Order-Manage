67f15e61ce326696d4223e04693bc14a2cc563a1
hugoc12 <hugo_c12@outlook.com>
2023-09-21 02:22:59 -0300

    ...

--- src/pages/pedidos/index.jsx
+++ src/pages/pedidos/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Container, Nav, Navbar, Table, Dropdown, DropdownButton, Button, Modal, Form, FormGroup, Row, Col, ListGroup } from 'react-bootstrap';
+import { Container, Nav, Navbar, Table, Dropdown, DropdownButton, Button, Modal, Form, FormGroup, Row, Col, ListGroup, FormLabel } from 'react-bootstrap';
 import './pedidos.css';
 
 export default function Pedidos() {
@@ -25,12 +25,10 @@ export default function Pedidos() {
             price: 23.40
         }
     ]
-
     const [show, setShow] = useState(false);
     const [validacaoForm, setValidacaoForm] = useState(false);
-    const [radioChecked, setRadioChecked] = useState({pessoaFisica: true, pessoaJuridica: false});
+    const [radioChecked, setRadioChecked] = useState({ pessoaFisica: true, pessoaJuridica: false });
     const [vlTotalItem, setvlTotalItem] = useState('28.00');
-
     /*const [shoppingList, setShoppingList] = useState([])*/
 
     function handleSubmit(event) {
@@ -86,76 +84,76 @@ export default function Pedidos() {
                 <Modal.Header closeButton><Modal.Title>Modal heading</Modal.Title></Modal.Header>
                 <Modal.Body>
                     <Form noValidate validated={validacaoForm} onSubmit={(event) => handleSubmit(event)} id='formIncluirPedido' name='formIncluirPedido'>
-                        <Form.Group className="mb-3" controlId='formPedidoTipoPessoa' style={{textAlign:'center', fontSize:'20px'}}>
+                        <Form.Group className="mb-3" controlId='formPedidoTipoPessoa' style={{ textAlign: 'center', fontSize: '20px' }}>
                             <Form.Check
+                                defaultChecked
                                 inline
-                                value={'pessoaFisica'}
-                                checked={radioChecked.pessoaFisica}
                                 label="Pessoa Física"
                                 name="tipoPessoa"
                                 type='radio'
                                 id={`pessoaFísica`}
-                                onClick={(e)=>setRadioChecked({pessoaFisica:true, pessoaJuridica:false})}
+                                onClick={(e) => setRadioChecked({ pessoaFisica: true, pessoaJuridica: false })}
                             />
                             <Form.Check
                                 inline
-                                value={'pessoaJuridica'}
-                                checked={radioChecked.pessoaJuridica}
                                 label="Pessoa Jurídica"
                                 name="tipoPessoa"
                                 type='radio'
                                 id={`pessoaJurídica`}
-                                onClick={(e)=>setRadioChecked({pessoaFisica:false, pessoaJuridica:true})}
+                                onClick={(e) => setRadioChecked({ pessoaFisica: false, pessoaJuridica: true })}
                             />
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formPedidoPrimeiroNome">
-                            <Form.Label>Primeiro Nome</Form.Label>
-                            <Form.Control required pattern='[A-Za-z ç]+' placeholder="Primeiro Nome" form='formIncluirPedido' name='primeiroNome' maxLength={60} />
+                        <Form.Group className="mb-3" controlId="formPedidoNome">
+                            <Form.Label>Nome</Form.Label>
+                            <Form.Control required pattern='[A-Za-z ç]+' placeholder="Nome" form='formIncluirPedido' name='primeiroNome' maxLength={60} />
                         </Form.Group>
-                        {radioChecked.pessoaFisica?
-                        <FormGroup className='mb-3' controlId='formPedidoUltimoNome'>
-                            <Form.Label>Último Nome</Form.Label>
-                            <Form.Control required pattern='[A-Za-z ç]+' placeholder='Último Nome' form='formIncluirPedido' name='ultimoNome' maxLength={60}></Form.Control>
-                        </FormGroup>
-                        :<></>}
+                        {radioChecked.pessoaFisica ?
+                            <FormGroup className='mb-3' controlId='formPedidoUltimoNome'>
+                                <Form.Label>Último Nome</Form.Label>
+                                <Form.Control required pattern='[A-Za-z ç]+' placeholder='Último Nome' form='formIncluirPedido' name='ultimoNome' maxLength={60}></Form.Control>
+                            </FormGroup>
+                            : <></>}
 
                         <Form.Group className="mb-3" controlId="formPedidoEmail">
                             <Form.Label>Endereço de Email</Form.Label>
-                            <Form.Control required type="email" pattern='[A-za-z]+@{1}[A-za-z]+.com(.br){0,1}' placeholder="Enter email" form='formIncluirPedido' name='email' />
+                            <Form.Control required type="email" placeholder="Enter email" form='formIncluirPedido' name='email' />
                             <Form.Control.Feedback type='invalid'>
                                 Email inválido!
                             </Form.Control.Feedback>
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formPedidoContato">
-                            <Form.Label>Telefone/Celular</Form.Label>
-                            <Form.Control required type='tel' maxLength={13} form='formIncluirPedido' name='numeroContato' aria-describedby="exemploNumero" />
-                            <Form.Text id="exemploNumero" muted>Ex: (11)1234-1234/(11)91234-1234</Form.Text>
-                        </Form.Group>
+                        <Row className="mb-2">
+                            <Form.Group md="6" as={Col} controlId="formPedidoTelefone">
+                                <Form.Label>Telefone</Form.Label>
+                                <Form.Control required type='tel' minLength={10} maxLength={10} pattern='[0-9]+' form='formIncluirPedido' name='telefone' />
+                                <Form.Control.Feedback type="invalid">Número de telefone inválido</Form.Control.Feedback>
+                            </Form.Group>
+                            <Form.Group md="6" as={Col} controlId="formPedidoCelular">
+                                <Form.Label>Celular</Form.Label>
+                                <Form.Control required type='tel' minLength={11} maxLength={11} pattern='[0-9]+' form='formIncluirPedido' name='celular' />
+                                <Form.Control.Feedback type="invalid">Número de celular inválido</Form.Control.Feedback>
+                            </Form.Group>
+                        </Row>
 
                         <Form.Group className="mb-3" controlId="formPedidoRegistro">
-                            <Form.Control required placeholder={`Digite seu ${radioChecked.pessoaFisica?'CPF':'CNPJ'}...`} maxLength={15} form='formIncluirPedido' name='registro' />
+                            <Form.Label>{radioChecked.pessoaFisica ? 'CPF' : 'CNPJ'}</Form.Label>
+                            <Form.Control required placeholder={`Digite seu ${radioChecked.pessoaFisica ? 'CPF' : 'CNPJ'}...`} maxLength={15} pattern='[0-9]+' form='formIncluirPedido' name='registro' />
                         </Form.Group>
 
-                        <Row className="mb-2">
-                            <FormGroup md={"10"} as={Col} controlId='formPedidoEndereco'>
+                        <Row className="mb-3">
+                            <FormGroup md={"9"} as={Col} controlId='formPedidoEndereco'>
                                 <Form.Label>Endereço</Form.Label>
-                                <Form.Control required placeholder='Digite seu endereço...' form='formIncluirPedido' name='endereco' />
+                                <Form.Control required placeholder='Digite seu endereço...' maxLength={60} form='formIncluirPedido' name='endereco' />
                             </FormGroup>
 
-                            <FormGroup md={"2"} as={Col} controlId='formPedidoEnderecoNumero'>
+                            <FormGroup md={"3"} as={Col} controlId='formPedidoEnderecoNumero'>
                                 <Form.Label>Numero</Form.Label>
-                                <Form.Control required placeholder='nº' minLength={1} maxLength={7} form='formIncluirPedido' name='numeroResidencial' />
+                                <Form.Control required placeholder='nº' minLength={1} maxLength={7} pattern='[0-9]+' form='formIncluirPedido' name='numeroResidencial' />
                             </FormGroup>
                         </Row>
 
                         <Row className="mb-3">
-                            <FormGroup as={Col} controlId="formPedidoCidade">
-                                <Form.Label>Cidade</Form.Label>
-                                <Form.Control required form='formIncluirPedido' name='cidade' />
-                            </FormGroup>
-
                             <FormGroup as={Col} controlId="formPedidoEstado">
                                 <Form.Label>Estado</Form.Label>
                                 <Form.Select required form='formIncluirPedido' name='estado'>
@@ -191,9 +189,14 @@ export default function Pedidos() {
                                 </Form.Select>
                             </FormGroup>
 
+                            <FormGroup as={Col} controlId="formPedidoCidade">
+                                <Form.Label>Cidade</Form.Label>
+                                <Form.Control required form='formIncluirPedido' name='cidade' pattern='[a-zA-z ç]+' maxLength={48}/>
+                            </FormGroup>
+
                             <FormGroup as={Col} controlId="formPedidoCep">
                                 <Form.Label>Cep</Form.Label>
-                                <Form.Control required maxLength={8} form='formIncluirPedido' name='cep' />
+                                <Form.Control required minLength={8} maxLength={8} pattern='[0-9]+' form='formIncluirPedido' name='cep' />
                             </FormGroup>
                         </Row>
 
